70695c0171b3436d242fa75f0962ab929316b0ce
E Folan <folanenda@gmail.com>
2017-05-16 00:16:26 +0100

    More notes and Console app connecting to blob storage

---
+++ Blobs/Blobs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Blobs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string storageconnection = System.Configuration.ConfigurationManager.AppSettings.Get("StorageConnectionString");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageconnection);
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer container = blobClient.GetContainerReference("objective2");
+
+            container.CreateIfNotExists();
+
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("examobjectives");
+
+            using (var fileStream = System.IO.File.OpenRead(@"C:\Users\Enda\Desktop\mofo.txt.txt"))
+            {
+                blockBlob.UploadFromStream(fileStream);
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
